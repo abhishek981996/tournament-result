55a9f55bbe34bf3c424f6bc7c424d2beb3c2c2b0
Abhishek tiwari <abhishektiwari981996@gmail.com>
2016-11-05 14:49:04 +0530

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -7,17 +7,21 @@
 -- these lines here.
 
 CREATE TABLE players (name Text,
-						id serial primary key, 
-						UNIQUE(id)
-						);
+			id serial primary key, 
+			UNIQUE(id)
+			);
+''' Table players contains column name (text) ,id (serial). id is use as a primary key inorder to establish foreign key 
+relationship'''			
+			
 CREATE TABLE matches(player_won int,
-						player_lost int,
-						NUM serial,
-						FOREIGN KEY(player_won) REFERENCES players(id),
-						FOREIGN KEY(player_lost) REFERENCES players(id)
-					);
+			player_lost int,
+			NUM serial,
+			FOREIGN KEY(player_won) REFERENCES players(id),
+			FOREIGN KEY(player_lost) REFERENCES players(id)
+			);
 
-
+'''a view is a virtual table based on the result-set of an SQL statement.
+ A view of players standing containing id ,name ,no of matches won , toatal matches played column are listed '''
 CREATE OR REPLACE VIEW standing AS
 	SELECT players.id, players.name,
 	sum(CASE WHEN players.id = matches.player_won THEN 1 ELSE 0 END) as won,
